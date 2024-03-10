5b1becccf0dd87966ca6258443b4d227555079dd
Justin Leighton <justinthomas.leighton@gmail.com>
2024-03-10 16:30:33 -0500

    update exception docstrings

--- mixmancer/exceptions.py
+++ mixmancer/exceptions.py
@@ -1,6 +1,10 @@
 class InvalidVolumeError(Exception):
+    """Invalid volume value passed to pygame mixer"""
+
     pass
 
 
 class InvalidChannelError(Exception):
+    """Invalid mixer channel passed to pygame mixer"""
+
     pass
